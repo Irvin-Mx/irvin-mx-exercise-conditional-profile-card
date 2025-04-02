86f2ff2e6b629594fc3502065487dd4e72c4df81
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-02 05:41:19 +0000

    Modified app.js file, manipulate linkedin variable

--- src/js/app.js
+++ src/js/app.js
@@ -28,7 +28,6 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  let linkedInName = variables.linkedin.split(" ").join("-");
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -59,7 +58,9 @@ function render(variables = {}) {
             <li><a href="/${
               variables.linkedin === null
                 ? "https://linkedin.com/school/4geeksacademy"
-                : `https://linkedin.com/in/${variables.linkedin}`
+                : `https://linkedin.com/in/${variables.linkedin
+                    .split(" ")
+                    .join("-")}`
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="${
               variables.instagram === null
@@ -70,7 +71,6 @@ function render(variables = {}) {
         </div>
     `;
 }
-
 
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
